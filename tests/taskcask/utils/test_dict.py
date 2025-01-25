5d625c504e6c68f9fb3ef155c699bcc64ce84452
Ignytis <155588001+ignytis@users.noreply.github.com>
2025-01-25 21:17:06 +0200

    Added test

--- tests/taskcask/utils/test_dict.py
+++ tests/taskcask/utils/test_dict.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from taskcask.utils.dict import dict_deep_merge
+from taskcask.utils.dict import dict_deep_merge, dict_unflatten
 
 
 class DictMergeTest(TestCase):
@@ -12,3 +12,11 @@ class DictMergeTest(TestCase):
         self.assertDictEqual(
             {"a": 1, "b": {"x": 10, "y": 50, "z": 40}, "c": 3, "d": 4},
             dict_deep_merge(dict1, dict2, dict3))
+
+
+class DictUnflattenTest(TestCase):
+    def test_unflatten(self) -> None:
+        self.assertDictEqual(
+            {"a": "one", "b": {"c": {"d": "two", "e": False}, "f": 4}},
+            dict_unflatten({"a": "one", "b.c.d": "two", "b.c.e": False, "b.f": 4}),
+        )
