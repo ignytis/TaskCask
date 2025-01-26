7cb02aa8d5cc930b671a0ec73758032c869ca62a
Ignytis <155588001+ignytis@users.noreply.github.com>
2025-01-27 00:55:42 +0200

    Task template definitions - autoloader

--- src/taskcask/stdlib/autoloader.py
+++ src/taskcask/stdlib/autoloader.py
@@ -1,3 +1,4 @@
 from .config import builder  # noqa
 from .executors import system_command  # noqa
 from .task_templates import system_command  # noqa
+from .task_templates.definition_loaders import dir_toml_loader  # noqa
--- src/taskcask/task_templates/definitions/factory.py
+++ src/taskcask/task_templates/definitions/factory.py
@@ -1,13 +1,12 @@
-from importlib.metadata import entry_points
-from typing import Generator, Type
+from typing import Generator
 
 from ...config.types import Config
 from .loader import BaseTaskTemplateDefinitionLoader
 from ...typedefs import TaskTemplateDefinition
+from ...utils.reflection import get_all_subclasses
 
 
 def get_task_template_definitions(config: Config) -> Generator[dict[str, TaskTemplateDefinition], None, None]:
-    for entry_point in entry_points(group="taskcask.task_templates.definition_loaders"):
-        cls: Type[BaseTaskTemplateDefinitionLoader] = entry_point.load()
+    for cls in get_all_subclasses(BaseTaskTemplateDefinitionLoader):
         for d in cls().load(config) or []:
             yield d
