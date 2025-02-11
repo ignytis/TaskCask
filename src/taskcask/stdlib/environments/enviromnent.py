2718dca99e7e8135c2a7b46e2c9c563c388801fd
Ignytis <155588001+ignytis@users.noreply.github.com>
2025-02-11 23:35:23 +0200

    Docker env

--- src/taskcask/stdlib/environments/enviromnent.py
+++ src/taskcask/stdlib/environments/enviromnent.py
@@ -1,19 +1,54 @@
+from enum import Enum
 from typing import Literal
 
 from ...environments.environment import BaseEnvironment
-from ...typedefs import StringKvDict
+from ...typedefs import EnvVars, BaseModel
 
 
 class LocalEnvironment(BaseEnvironment):
+    """
+    Local machine
+    """
     kind: Literal["local"] = "local"
-    env: StringKvDict
+    env: EnvVars
     """Environment variables"""
 
 
 class SshEnvironment(BaseEnvironment):
+    """
+    Remote server (SSH connection)
+    """
     kind: Literal["ssh"] = "ssh"
-    env: StringKvDict
+    env: EnvVars
     host: str
     port: str = "22"
     user: str
     """Environment variables"""
+
+
+class DockerEnvironmentVolumeMountMode(Enum):
+    ro = "ro"
+    """Read-only mode"""
+    rw = "rw"
+    """Read and Write mode"""
+
+
+class DockerEnvironmentVolume(BaseModel):
+    host_path: str
+    """path the host system"""
+    container_path: str
+    """path inside container"""
+    mode: DockerEnvironmentVolumeMountMode = DockerEnvironmentVolumeMountMode.ro
+
+
+class DockerEnvironment(BaseEnvironment):
+    """
+    Remote server (SSH connection)
+    """
+    kind: Literal["docker"] = "docker"
+    env: EnvVars
+    """Environment variables"""
+    image: str
+    """Image to use"""
+    volumes: list[DockerEnvironmentVolume] = []
+    """Volumes to mount"""
--- src/taskcask/stdlib/executors/system_command.py
+++ src/taskcask/stdlib/executors/system_command.py
@@ -1,16 +1,23 @@
 import logging
 import subprocess
+import shlex
 from typing import Any
 
 from ...environments.environment import BaseEnvironment
 from ...executors.executor import BaseExecutor
 from ...task import Task
 from ...stdlib.task_templates.system_command import SystemCommandTaskTemplate
-from ..environments.enviromnent import LocalEnvironment, SshEnvironment
+from ..environments.enviromnent import DockerEnvironment, LocalEnvironment, SshEnvironment
+
+
+_SUPPORTED_ENV_CLASSES = [DockerEnvironment, LocalEnvironment, SshEnvironment]
 
 
 def _is_supported_env(env: BaseEnvironment) -> bool:
-    return isinstance(env, LocalEnvironment) or isinstance(env, SshEnvironment)
+    for cls in _SUPPORTED_ENV_CLASSES:
+        if isinstance(env, cls):
+            return True
+    return False
 
 
 class SystemCommandExecutor(BaseExecutor):
@@ -24,20 +31,45 @@ class SystemCommandExecutor(BaseExecutor):
             and _is_supported_env(env)
 
     def execute(self, task: Task, env: BaseEnvironment) -> Any:
-        if isinstance(env, LocalEnvironment):
+        if isinstance(env, DockerEnvironment):
+            return self._run_docker(task, env)
+        elif isinstance(env, LocalEnvironment):
             return self._run_locally(task, env)
         elif isinstance(env, SshEnvironment):
             return self._run_ssh(task, env)
         else:
             raise NotImplementedError(f"Unsupported target environment: {env.kind}")
 
+    def _run_docker(self, task: Task, env: DockerEnvironment):
+        """
+        Run a system command in Docker containers
+        """
+        try:
+            import docker
+        except ImportError:
+            raise Exception("Cannot import Docker. Please install the application with 'docker' extra")
+        tpl: SystemCommandTaskTemplate = task.template
+        cmd = tpl.cmd + task.args
+        env_dict = {**env.env, **tpl.env}
+        command_string = _format_command(cmd)
+
+        client = docker.from_env()
+        return client.containers.run(image=env.image, command=command_string, entrypoint=None, environment=env_dict) \
+            .decode("utf-8")
+
     def _run_locally(self, task: Task, env: LocalEnvironment):
+        """
+        Run a system command as a local process
+        """
         tpl: SystemCommandTaskTemplate = task.template
         cmd = tpl.cmd + task.args
         env_dict = {**env.env, **tpl.env}
         return subprocess.check_output(cmd, env=env_dict).decode("utf-8")
 
     def _run_ssh(self, task: Task, env: SshEnvironment):
+        """
+        Run a system command on a remove server using SSH
+        """
         try:
             from fabric import Connection
         except ImportError:
@@ -51,3 +83,10 @@ class SystemCommandExecutor(BaseExecutor):
         with Connection(env.host, user=env.user, port=env.port) as c:
             result = c.run(command_string, hide=True, env=env_dict)
         return result.stdout
+
+
+def _format_command(args: list[str]) -> str:
+    """
+    Formats a string command from list of strings
+    """
+    return " ".join(shlex.quote(arg) for arg in args)
